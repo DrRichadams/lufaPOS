147dc6e82524bd7ffc376c7dd289d45e78c6c5ad
DrRichAdams <proxyserver7798@gmail.com>
2024-04-29 22:35:05 +0200

    Defined structure

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import { Pressable, View, StyleSheet } from 'react-native';
 
 
 export default function TabLayout() {
 
   return (
-    <Tabs>
+    <Tabs screenOptions={{
+        title: "LUFA MEDIA POS",
+        headerTitleAlign: "center",
+        headerRight: () => <Pressable onPress={() => console.warn("Pressed")}><FontAwesome name='user-circle' size={30} style={styles.order_title_box} /></Pressable>,
+    }}>
+      
       <Tabs.Screen name="index"/>
-      <Tabs.Screen name="two" />
+      <Tabs.Screen name="order" options={{}} />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  order_title_box: {
+    paddingHorizontal: 10
+  }
+});
---
+++ app/(tabs)/order.tsx
@@ -0,0 +1,13 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function TabTwoScreen() {
+  return (
+    <View>
+      <Text>Tab Two</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
